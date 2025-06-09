3636a167e0146c18fdcd92d0d3794741aaa79b92
TimTreg <timothytregilgas@suttongrammar.school>
2025-06-09 10:30:04 +0100

    Update dijkstraCalculator.java

--- code/dijkstraCalculator.java
+++ code/dijkstraCalculator.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class dijkstraCalculator {
@@ -9,44 +11,48 @@ public class dijkstraCalculator {
         // Split the expression by spaces
         String[] tokens = expression.split(" ");
         for (String token : tokens) {
-            switch (token) {
-                case "(":
-                    break;
-                case "+":
-                case "-":
-                case "*":
-                case "/":
-                    operators.push(token);
-                    break;
-                case ")":
-                    String op = operators.pop();
-                    double right = operands.pop();
-                    double left = operands.pop();
-                    double result=0;
-                    switch (op)
-                    {
-                        case "+": result =  left + right;break;
-                        case "-": result =  left - right;break;
-                        case "*": result =  left * right;break;
-                        case "/": result =  left / right;break;
-                        default: throw new IllegalArgumentException("Unknown operator: " + op);
-                    };
-                    operands.push(result);
-                    break;
-                default:
-                    // Assume it's a number
-                    operands.push(Double.parseDouble(token));
-                    break;
+            if (token.equals("+") || token.equals("-") || token.equals("*") || token.equals("/")) {
+                operators.push(token);
+            } else if (token.equals(")")) {
+                String op = operators.pop();
+                double right = operands.pop();
+                double left = operands.pop();
+                double result = 0;
+                switch (op) {
+                    case "+":
+                        result = left + right;
+                        break;
+                    case "-":
+                        result = left - right;
+                        break;
+                    case "*":
+                        result = left * right;
+                        break;
+                    case "/":
+                        result = left / right;
+                        break;
+                    default:
+                        throw new IllegalArgumentException("Unknown operator: " + op);
+                }
+                ;
+                operands.push(result);
+            } else if (!token.equals("(")){
+                operands.push(Double.parseDouble(token));
             }
         }
 
         return operands.pop();
     }
 
-    public static void main(String[] args) {
-        String expr = "( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )";
-        double result = evaluate(expr);
-        System.out.println("Result: " + result); // Should print 101.0
-    }
+
+
+public static void main(String[] args) {
+    String expr;
+    System.out.println("Enter expression: ");
+    Scanner in = new Scanner(System.in);
+    expr= in.nextLine();
+    double result = evaluate(expr);
+    System.out.println("Result: " + result); 
+}
 }
 
